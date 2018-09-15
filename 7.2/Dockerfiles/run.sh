15678b3d6699287439caa1b20e9dd6504b05b328
Sebastian Kalicki <s.kalicki@whow.net>
2018-09-15 22:30:05 +0200

    public directory ... XD

--- 7.2/Dockerfiles/run.sh
+++ 7.2/Dockerfiles/run.sh
@@ -50,6 +50,7 @@ function create_public_directory {
     if [ ! -z ${PUBLIC_DIRECTORY} ]; then
         PUBLIC_DIRECTORY_PATH=${SERVER_ROOT}${HTDOCS}/${PUBLIC_DIRECTORY}
         sed -i "s/\"\/var\/www\/localhost\/htdocs\"/\"\/var\/www\/localhost\/htdocs\/${PUBLIC_DIRECTORY}\"/" ${APACHE_ROOT}/httpd.conf
+        sed -i "s/\"\/var\/www\/localhost\/htdocs\"/\"\/var\/www\/localhost\/htdocs\/${PUBLIC_DIRECTORY}\"/" ${APACHE_ROOT}/conf.d/ssl.conf
 
         if [[ ! -d ${PUBLIC_DIRECTORY_PATH} ]]; then
             mkdir ${PUBLIC_DIRECTORY_PATH}
