fe2f307fc8333669c0c0d130d4503470fc1f0911
Sebastian Kalicki <sebastian@kalicki.email>
2023-01-22 22:40:16 +0100

    Added 7.4

---
+++ 7.4/Dockerfiles/var/www/skel/htdocs/index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="de">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro:200,400" rel="stylesheet">
+    <style type="text/css">
+        html, body {
+            background-color: #30353E;
+            height: 100%;
+            margin: 0; }
+
+        body {
+            -webkit-box-align: center;
+            -ms-flex-align: center;
+            align-items: center;
+            display: -webkit-box;
+            display: -ms-flexbox;
+            display: flex;
+            -webkit-box-pack: center;
+            -ms-flex-pack: center;
+            justify-content: center; }
+    </style>
+</head>
+<body>
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 145 145" height="145" width="145"><defs><style>.cls-1{fill:#babec4;}.cls-2{opacity:0.05;}.cls-3{clip-path:url(#clip-path);}.cls-4{clip-path:url(#clip-path-2);}</style><clipPath id="clip-path"><polygon class="cls-1" points="108.75 63.44 67.97 104.22 108.75 145 145 145 104.22 104.22 145 63.44 108.75 63.44"/></clipPath><clipPath id="clip-path-2"><polygon class="cls-1" points="27.19 0 0 27.19 0 145 22.66 122.34 45.31 145 81.56 145 40.78 104.22 81.56 63.44 45.31 63.44 27.19 81.56 27.19 0"/></clipPath></defs><title>Zeichenfläche 1</title><g id="Logo_2" data-name="Logo 2" class="cls-2"><polygon class="cls-1" points="108.75 63.44 67.97 104.22 108.75 145 145 145 104.22 104.22 145 63.44 108.75 63.44"/><g class="cls-3"><rect class="cls-1" width="145" height="145"/></g><polygon class="cls-1" points="27.19 0 0 27.19 0 145 22.66 122.34 45.31 145 81.56 145 40.78 104.22 81.56 63.44 45.31 63.44 27.19 81.56 27.19 0"/><g class="cls-4"><rect class="cls-1" width="145" height="145"/></g></g></svg>
+</body>
+</html>
